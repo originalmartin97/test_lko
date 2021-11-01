f3671239e4c2ddcdbdf70f9bbf5081e70edd822b
originalmartin97 <originalmartin97@gmail.com>
2021-11-01 14:36:04 +0100

    Fix error & remove using

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-using namespace std;
 
 // Super awesome greatest common driver program
 int main(){
     int a,b;
-    cin >> a >> b; // Reading the two numbers from the standard input
+    std::cin >> a >> b; // Reading the two numbers from the standard input
     int lko =1;
     int& min = (a<b)?a:b;
     for(int i = 1; i < min; i++){
@@ -12,6 +11,6 @@ int main(){
             lko = i;
         }
     }
-    cout << lko << endl // Printing the gcd to STOUT
+    std::cout << lko << std::endl; // Printing the gcd to STOUT
     return 0;
 }
