16a4a21ff7ce43926052831c982131ecdf908dc6
originalmartin97 <originalmartin97@gmail.com>
2021-11-01 21:47:56 +0100

    Fix GCD algorithm

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(){
     std::cin >> a >> b; // Reading the two numbers from the standard input
     int lko =1;
     int& min = (a<b)?a:b;
-    for(int i = 1; i < min; i++){
+    for(int i = 1; i <= min; i++){
         if(a%i==0 && b%i==0){
             lko = i;
         }
